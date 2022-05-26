ad9cbf2204aaa0bf15621350516b075a9f84d705
Ganza Ntambuko <ganzantambuko@gmail.com>
2022-05-26 12:52:18 +0200

    menu rigth

--- js/script.js
+++ js/script.js
@@ -16,9 +16,3 @@ document.querySelectorAll('.about .video-container .controls .control-btn').forE
 })
 
 
-document.querySelectorAll('#ourstory .video-container .controls .control-btn').forEach(btn =>{
-    btn.onclick = () =>{
-        let src = btn.getAttribute('data-src');
-        document.querySelector('#ourstory .video-container .video').src = src;
-    }
-})
